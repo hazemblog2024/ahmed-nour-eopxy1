1b3603681731ca879eecca948d7d988977f5b199
hazemblog2024 <hazemblog2024@gmail.com>
2025-05-28 17:56:45 +0300

    تحديث sitemap وrobots للدومين الجديد www.3azlepoxy.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://ahmed-nour-eopxy1.vercel.app';
+  const baseUrl = 'https://www.3azlepoxy.com';
 
   return {
     rules: [
