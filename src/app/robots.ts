8b6fb6fc79c40855149ebed47b679b70b6fa78b6
hazemblog2024 <hazemblog2024@gmail.com>
2025-05-28 16:20:03 +0300

    إنشاء ملفات sitemap.ts وrobots.ts ديناميكية بالنطاق الصحيح

---
+++ src/app/robots.ts
@@ -0,0 +1,41 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://ahmed-nour-eopxy1.vercel.app';
+
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/private/',
+          '/_next/',
+          '/static/',
+        ],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/private/',
+        ],
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/private/',
+        ],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
